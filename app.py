4a34df1e3db8663ffadb68520c999c5ff57b19db
Nagraj <nagrajtham@gmail..com>
2023-12-20 15:04:12 +0530

    Changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from difflib import get_close_matches
+import os
 
 app = Flask(__name__)
 
@@ -47,4 +48,6 @@ def autocorrect_api():
     return jsonify({'original_sentence': sentence, 'corrected_sentence': corrected_sentence})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
